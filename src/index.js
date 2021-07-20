31dfcbf35dd01d4cbbc9bc749e1c906920ae7680
VitaliSantalau <v.santalau@gmail.com>
2021-07-20 23:38:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  n = n < 0 ? -n : n;
+  return  `${n}`
+    .split('')
+    .reduce((acc, _elem, i , arr) => {
+      acc[i] = arr[(arr.length - 1) - i];
+      return acc;
+    }, [])
+    .join('');
 }
